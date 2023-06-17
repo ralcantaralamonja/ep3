cf9b2b574fbc3d99437e7628c85502ebeaf6b109
Franco1097 <gian.franco.lozano@hotmail.com>
2023-06-16 23:27:17 -0500

    creacion de package y clientecontroller

--- src/main/java/com/ep3/grupo5/repository/ClienteRepository.java
+++ src/main/java/com/ep3/grupo5/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
-package com.ep3.grupo5;
+package com.ep3.grupo5.repository;
 
+import com.ep3.grupo5.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
