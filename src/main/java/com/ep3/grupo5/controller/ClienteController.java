c1e5ee5d5ceb8af413ce05e6b872d5b1141cbb63
=Ana Airas <=anaar131202@gmail.com>
2023-06-16 23:42:16 -0500

    editar y eliminar controller

--- src/main/java/com/ep3/grupo5/controller/ClienteController.java
+++ src/main/java/com/ep3/grupo5/controller/ClienteController.java
@@ -3,13 +3,47 @@ package com.ep3.grupo5.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/editar")
+    public String editar(@RequestParam("id") Long id, Model model){
+        model.addAttribute("alumno", clienteService.obtenerPorId(id));
+        return "cliente/editar";
+    }
+
+    @DeleteMapping("/eliminar")
+    public String eliminar(@ModelAttribute("id") Long id){
+        clienteService.eliminar(id);
+        return "redirect:/cliente";
+    }
 
 }
