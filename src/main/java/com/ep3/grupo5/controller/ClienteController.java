378c86b09a358f522c05c5ae49de13cb6a6e871b
Franco1097 <gian.franco.lozano@hotmail.com>
2023-06-16 23:38:26 -0500

    Agregando registro

--- src/main/java/com/ep3/grupo5/controller/ClienteController.java
+++ src/main/java/com/ep3/grupo5/controller/ClienteController.java
@@ -1,15 +1,31 @@
 package com.ep3.grupo5.controller;
 
+import com.ep3.grupo5.entity.Cliente;
+import com.ep3.grupo5.service.ClienteService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @GetMapping("")
+    public String index(Model model){
+        model.addAttribute("clientes", clienteService.obtenerTodos());
+        return "/cliente";
+    }
+
+    @PutMapping("")
+    public String registrar(@ModelAttribute("cliente")Cliente cliente){
+        clienteService.registrar(cliente);
+        return "redirect/cliente";
+    }
+
+
 
 }
