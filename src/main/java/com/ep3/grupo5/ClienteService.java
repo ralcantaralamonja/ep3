bfea9e2fe06f32e55be34592533943051ba56014
=Ana Airas <=anaar131202@gmail.com>
2023-06-16 23:05:37 -0500

    Cliente Service

---
+++ src/main/java/com/ep3/grupo5/ClienteService.java
@@ -0,0 +1,45 @@
+package com.ep3.grupo5;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@AllArgsConstructor
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public List<Cliente> obtenerTodos(){
+        return clienteRepository.findAll();
+    }
+
+    public Cliente obtenerporId(Long id) throws Exception {
+        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+        if (optionalCliente.isPresent()){
+            return optionalCliente.get();
+        }else{
+            throw new Exception("El alumno no existe");
+        }
+    }
+
+    public Cliente registrar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public void eliminar(Long id) throws Exception {
+
+        Optional<Cliente> optionalCliente= clienteRepository.findById(id);
+
+        if (optionalCliente.isPresent()){
+            clienteRepository.delete(optionalCliente.get());
+        }else{
+            throw new Exception("El alumno no existe");
+        }
+
+    }
+
+}
